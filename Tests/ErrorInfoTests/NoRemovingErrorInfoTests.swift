400ab86fad5198fb1437aa9400343d102716f20e
Dmitriy Ignatyev <dy.ignatyev@gmail.com>
2025-09-22 06:43:18 +0300

    test files

---
+++ Tests/ErrorInfoTests/NoRemovingErrorInfoTests.swift
@@ -0,0 +1,12 @@
+//
+//  NoRemovingErrorInfoTests.swift
+//  swifty-kit
+//
+//  Created by Dmitriy Ignatyev on 16/09/2025.
+//
+
+import ErrorInfo
+
+struct NoRemovingErrorInfoTests {
+  func testNoRemove() {}
+}
