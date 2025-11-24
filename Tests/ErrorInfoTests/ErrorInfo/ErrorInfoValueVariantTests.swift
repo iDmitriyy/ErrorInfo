678cc2738b121294dacbeda17c859ca5d58fb9d8
Dmitriy Ignatyev <21144498+iDmitriyy@users.noreply.github.com>
2025-11-25 01:50:45 +0300

    ErrorInfo._ValueVariant basic tests

---
+++ Tests/ErrorInfoTests/ErrorInfo/ErrorInfoValueVariantTests.swift
@@ -0,0 +1,30 @@
+//
+//  ErrorInfoValueVariantTests.swift
+//  ErrorInfo
+//
+//  Created by Dmitriy Ignatyev on 25/11/2025.
+//
+
+@testable import ErrorInfo
+import Testing
+
+struct ErrorInfoValueVariantTests {
+  typealias Variant = ErrorInfo._ValueVariant
+  
+  @Test func equality() async throws {
+    #expect(Variant.isApproximatelyEqual(lhs: .value(10), rhs: .value(10)))
+    #expect(!Variant.isApproximatelyEqual(lhs: .value(Int(10)), rhs: .value(UInt(10))))
+    
+    #expect(!Variant.isApproximatelyEqual(lhs: .value(10),
+                                         rhs: .nilInstance(typeOfWrapped: Int.self)))
+    
+    #expect(Variant.isApproximatelyEqual(lhs: .nilInstance(typeOfWrapped: Int.self),
+                                         rhs: .nilInstance(typeOfWrapped: Int.self)))
+    
+    #expect(!Variant.isApproximatelyEqual(lhs: .nilInstance(typeOfWrapped: Int.self),
+                                         rhs: .nilInstance(typeOfWrapped: UInt.self)))
+    
+    #expect(Variant.isApproximatelyEqual(lhs: .nilInstance(typeOfWrapped: Int.self as any ErrorInfoValueType.Type),
+                                         rhs: .nilInstance(typeOfWrapped: Int.self)))
+  }
+}
