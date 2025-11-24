b38e7b8b6d92b7c2ebb58cb8d6297aaf997c9317
Dmitriy Ignatyev <21144498+iDmitriyy@users.noreply.github.com>
2025-11-25 01:25:54 +0300

    tests compile

--- Tests/ErrorInfoTests/ToyExamples/MultipleValuesForKeyErrorInfoTypes/MultiValueErrorInfo.swift
+++ Tests/ErrorInfoTests/ToyExamples/MultipleValuesForKeyErrorInfoTypes/MultiValueErrorInfo.swift
@@ -9,29 +9,38 @@ import ErrorInfo
 import OrderedCollections
 
 /// If a key collision happens, the values are put into a container
-struct MultiValueErrorInfo: IterableErrorInfo {
-  typealias Key = String
-  typealias Value = any ErrorInfoValueType
-  typealias Element = (key: Key, value: Value)
+//struct MultiValueErrorInfo: IterableErrorInfo {
+//  typealias Key = String
+//  typealias Value = any ErrorInfoValueType
+//  typealias Element = (key: Key, value: Value)
+//  
+//  private typealias MultiValueStorage = OrderedMultiValueErrorInfoGeneric<Key, Value>
+//  
+//  private var _storage: MultiValueStorage
+//  
+//  init() {
+//    _storage = MultiValueStorage()
+//  }
+//  
+//  func makeIterator() -> some IteratorProtocol<Element> {
+//    var iterator = _storage.makeIterator()
+//    return AnyIterator {
+//      guard let (key, warppedValue) = iterator.next() else { return nil }
+//      return (key, warppedValue.value)
+//    }
+//  }
+//  
+//  mutating func addResolvingCollisions(key: Key, value: Value, insertIfEqual: Bool) {
+//    _storage.appendResolvingCollisions(key: key, value: value, insertIfEqual: insertIfEqual,
+//                                       collisionSource: .onSubscript(keyKind: .dynamic)) // TODO: collisionSource
+//  }
+//}
+
+struct AnyErrorInfoValue: ApproximatelyEquatable {
+  let wrapped: any ErrorInfoValueType
   
-  private typealias MultiValueStorage = OrderedMultiValueErrorInfoGeneric<Key, Value>
-  
-  private var _storage: MultiValueStorage
-  
-  init() {
-    _storage = MultiValueStorage()
-  }
-  
-  func makeIterator() -> some IteratorProtocol<Element> {
-    var iterator = _storage.makeIterator()
-    return AnyIterator {
-      guard let (key, warppedValue) = iterator.next() else { return nil }
-      return (key, warppedValue.value)
-    }
-  }
-  
-  mutating func addResolvingCollisions(key: Key, value: Value, insertIfEqual: Bool) {
-    _storage.appendResolvingCollisions(key: key, value: value, insertIfEqual: insertIfEqual,
-                                       collisionSource: .onSubscript(keyKind: .dynamic)) // TODO: collisionSource
+  static func isApproximatelyEqual(lhs: borrowing AnyErrorInfoValue, rhs: borrowing AnyErrorInfoValue) -> Bool {
+    ErrorInfoFuncs.isApproximatelyEqualAny(lhs, rhs)
   }
 }
+
--- Tests/ErrorInfoTests/ToyExamples/MultipleValuesForKeyErrorInfoTypes/StaticStringKeyErrorInfo.swift
+++ Tests/ErrorInfoTests/ToyExamples/MultipleValuesForKeyErrorInfoTypes/StaticStringKeyErrorInfo.swift
@@ -8,51 +8,51 @@
 import ErrorInfo
 import OrderedCollections
 
-struct StaticStringKeyErrorInfo: Sequence {
-  typealias Key = StaticString
-  typealias Value = any ErrorInfoValueType
-  typealias Element = (key: Key, value: Value)
-  
-  private typealias MultiValueStorage = OrderedMultiValueErrorInfoGeneric<StaticStringHashableAdapter, Value>
-  
-  private var _storage: MultiValueStorage
-  
-  func makeIterator() -> some IteratorProtocol<Element> {
-    var iterator = _storage.makeIterator()
-    return AnyIterator {
-      guard let (key, warppedValue) = iterator.next() else { return nil }
-      return (key.base, warppedValue.value)
-    }
-  }
-}
-
-// MARK: StaticString Hashable Adapter
-
-fileprivate struct StaticStringHashableAdapter: Hashable {
-  let base: StaticString
-  
-  init(_ wrappedValue: StaticString) {
-    self.base = wrappedValue
-  }
-  
-  func hash(into hasher: inout Hasher) {
-    base.withUTF8Buffer { utf8Buffer in
-      for uint8 in utf8Buffer {
-        hasher.combine(uint8)
-      }
-    }
-  }
-
-  // TODO: this is not proper imp
-  static func == (lhs: StaticStringHashableAdapter, rhs: StaticStringHashableAdapter) -> Bool {
-    lhs.base.withUTF8Buffer { lhsBuffer in
-      rhs.base.withUTF8Buffer { rhsBuffer in
-        guard lhsBuffer.count == rhsBuffer.count else { return false }
-        
-        return lhsBuffer.enumerated().allSatisfy { index, byte in
-          byte == rhsBuffer[index]
-        }
-      }
-    }
-  }
-}
+//struct StaticStringKeyErrorInfo: Sequence {
+//  typealias Key = StaticString
+//  typealias Value = any ErrorInfoValueType
+//  typealias Element = (key: Key, value: Value)
+//  
+//  private typealias MultiValueStorage = OrderedMultiValueErrorInfoGeneric<StaticStringHashableAdapter, Value>
+//  
+//  private var _storage: MultiValueStorage
+//  
+//  func makeIterator() -> some IteratorProtocol<Element> {
+//    var iterator = _storage.makeIterator()
+//    return AnyIterator {
+//      guard let (key, warppedValue) = iterator.next() else { return nil }
+//      return (key.base, warppedValue.value)
+//    }
+//  }
+//}
+//
+//// MARK: StaticString Hashable Adapter
+//
+//fileprivate struct StaticStringHashableAdapter: Hashable {
+//  let base: StaticString
+//  
+//  init(_ wrappedValue: StaticString) {
+//    self.base = wrappedValue
+//  }
+//  
+//  func hash(into hasher: inout Hasher) {
+//    base.withUTF8Buffer { utf8Buffer in
+//      for uint8 in utf8Buffer {
+//        hasher.combine(uint8)
+//      }
+//    }
+//  }
+//
+//  // TODO: this is not proper imp
+//  static func == (lhs: StaticStringHashableAdapter, rhs: StaticStringHashableAdapter) -> Bool {
+//    lhs.base.withUTF8Buffer { lhsBuffer in
+//      rhs.base.withUTF8Buffer { rhsBuffer in
+//        guard lhsBuffer.count == rhsBuffer.count else { return false }
+//        
+//        return lhsBuffer.enumerated().allSatisfy { index, byte in
+//          byte == rhsBuffer[index]
+//        }
+//      }
+//    }
+//  }
+//}
