1f349bbae20f753c918b4839545e06b77149f2d1
Dmitriy Ignatyev <21144498+iDmitriyy@users.noreply.github.com>
2025-11-26 00:21:09 +0300

    isEqualEqatableExistential

--- Tests/ErrorInfoTests/Functions/IsEqualEqatableExistentialTests.swift
+++ Tests/ErrorInfoTests/Functions/IsEqualEqatableExistentialTests.swift
@@ -11,8 +11,22 @@ import Testing
 struct IsEqualEqatableExistentialTests {
   @Test func basic() throws {
     #expect(ErrorInfoFuncs.isEqualEqatableExistential(a: Int(1), b: Int(1)) == true)
+    #expect(ErrorInfoFuncs.isEqualEqatableExistential(a: Int(1) as any ErrorInfoValueType,
+                                                      b: Int(1) as any ErrorInfoValueType) == true)
     
-    #expect(ErrorInfoFuncs.isEqualEqatableExistential(a: Int(1), b: UInt(1)) == false)
-    #expect(ErrorInfoFuncs.isEqualEqatableExistential(a: UInt(1), b: Int(1)) == false)
+    #expect(ErrorInfoFuncs.isEqualEqatableExistential(a: Int(1),
+                                                      b: UInt(1)) == false)
+    #expect(ErrorInfoFuncs.isEqualEqatableExistential(a: UInt(1),
+                                                      b: Int(1)) == false)
+    
+    #expect(ErrorInfoFuncs.isEqualEqatableExistential(a: Int(1) as any ErrorInfoValueType,
+                                                      b: UInt(1)) == false)
+    #expect(ErrorInfoFuncs.isEqualEqatableExistential(a: UInt(1) as any ErrorInfoValueType,
+                                                      b: Int(1)) == false)
+    
+    #expect(ErrorInfoFuncs.isEqualEqatableExistential(a: Int(1) as any BinaryInteger & Sendable,
+                                                      b: UInt(1)) == false)
+    #expect(ErrorInfoFuncs.isEqualEqatableExistential(a: UInt(1) as any BinaryInteger & Sendable,
+                                                      b: Int(1)) == false)
   }
 }
