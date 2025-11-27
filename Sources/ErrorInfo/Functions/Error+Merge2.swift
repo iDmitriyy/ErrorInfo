8ba065014e1665560fad79ed67680cc05c7259ca
Dmitriy Ignatyev <21144498+iDmitriyy@users.noreply.github.com>
2025-11-27 18:20:03 +0300

    Merge namespace

--- Sources/ErrorInfo/Functions/Error+Merge2.swift
+++ Sources/ErrorInfo/Functions/Error+Merge2.swift
@@ -15,12 +15,14 @@ struct ProtoError {
   let info: OrderedDictionary<String, Int>
 }
 
+/// Add partial functionality of collisions resolution to dictionary
+struct DictionaryErrorInfoOverlay<Dict> {
+  private(set) var dictionary: Dict
+}
+
 func playMerge(errors: [any InformativeError]) {
   _ = errors
 }
-
-// !! there should be an ability te remove duplicated values for the same key inside / across errorInfoSources, but the
-// knowledge that this duplicates happened should be put to the result dictionary.
 
 // Should ErrorInfo be codable? does json allow the same key several times?
 // ? Decode merged OrderedDictionary as Swift.Dectionary but with key-value ordering
@@ -29,21 +31,23 @@ enum MerrorInfoSourcesOptions {
   // omitEqualValuesInsideSource
   // omitEqualValuesAcrossSources
   // collapseNilValues
-  // addKeyTagsForKinds(.all, .allExceptLiterals | .literal, .combinedLiteral, .dynamic, .keyPath, .madified)
 }
 
+public enum Merge {}
 
-struct MerrorInfoKeyTagOptions {}
-
-struct MerrorInfoNilOptions {
-  // keepAll
-  // collapse | with info is it a nil or a collapsed nil. "nil" "nil (collapsed)" "nil (collapsed, was: Int, String, Array)"
+extension Merge {
+  struct KeyTagOptions {
+    // addKeyTagsForKinds(.allKeyKinds, .allExceptLiterals | .literal, .combinedLiteral, .dynamic, .keyPath, .madified)
+  }
+  
+  struct NilOptions {
+    // keepAll
+    // collapse | with info is it a nil or a collapsed nil. "nil" "nil (collapsed)" "nil (collapsed, was: Int, String, Array)"
+  }
 }
 
-/// Add partial functionality of collisions resolution to dictionary
-struct DictionaryErrorInfoOverlay<Dict> {
-  private(set) var dictionary: Dict
-}
+// !! there should be an ability te remove duplicated values for the same key inside / across errorInfoSources, but the
+// knowledge that this duplicates happened should be put to the result dictionary.
 
 // Before logging, typically some fields are added to summary info: "#log_file_line", "#log_throttling" ...
 // Such info can be added to a separate ErrorInfo instance and put first to `errorInfoSources` arg list.
@@ -60,88 +64,90 @@ struct DictionaryErrorInfoOverlay<Dict> {
 // - Swift.Error
 // - ErrorsSuite
 
-func summaryInfoMerge<E, V>(
-  errorInfoSources: some BidirectionalCollection<E>, // TODO: .reversed support | tests
-  errorInfoKeyPath: KeyPath<E, ErrorInfo>,
-  omitEqualValues: Bool, // = false
-  errorSignatureBuilder: (E) -> String,
-  valueTransformer: (ErrorInfo._ValueVariant) -> V,
-  collisionSourceInterpolation: (StringBasedCollisionSource
-  ) -> String = { $0.defaultStringInterpolation() },
-)
-  -> OrderedDictionary<String, V> {
-  // any ErrorInfoValueType change to V (e.g. to be Optional<any ErrorInfoValueType> or String)
-  typealias Key = String
-  typealias Value = any ErrorInfoValueType
-  
-  var summary: OrderedDictionary<Key, V> = [:]
-  
-  func putResolvingCollisions(key assumeModifiedKey: Key, value processedValue: V) {
-    ErrorInfoDictFuncs.Merge._putResolvingWithRandomSuffix(processedValue,
-                                                           assumeModifiedKey: assumeModifiedKey,
-                                                           shouldOmitEqualValue: omitEqualValues,
-                                                           suffixFirstChar: ErrorInfoMerge.suffixBeginningForMergeScalar,
-                                                           to: &summary)
+extension Merge {
+  func summaryInfo<E, V>(
+    errorInfoSources: some BidirectionalCollection<E>, // TODO: .reversed support | tests
+    errorInfoKeyPath: KeyPath<E, ErrorInfo>,
+    errorSignatureBuilder: (E) -> String,
+    valueTransform: (ErrorInfo._ValueVariant) -> V,
+    collisionSourceInterpolation: (StringBasedCollisionSource
+    ) -> String = { $0.defaultStringInterpolation() },
+  )
+    -> OrderedDictionary<String, V> {
+    // any ErrorInfoValueType change to V (e.g. to be Optional<any ErrorInfoValueType> or String)
+    typealias Key = String
+    typealias Value = any ErrorInfoValueType
+    
+    var summary: OrderedDictionary<Key, V> = [:]
+    
+    func putResolvingCollisions(key assumeModifiedKey: Key, value processedValue: V) {
+      ErrorInfoDictFuncs.Merge._putResolvingWithRandomSuffix(processedValue,
+                                                             assumeModifiedKey: assumeModifiedKey,
+                                                             shouldOmitEqualValue: false,
+                                                             suffixFirstChar: ErrorInfoMerge.suffixBeginningForMergeScalar,
+                                                             to: &summary)
+    }
+    
+    // 1. Find collisions across errorInfo sources (typically it is errors)
+    let crossCollisionKeys = findCommonElements(across: errorInfoSources.map { $0[keyPath: errorInfoKeyPath].uniqueKeys })
+    
+    // 2. Iterate over key-value pairs in each errorInfo source.
+    lazy var allSourcesSignatures = errorInfoSources.map(errorSignatureBuilder)
+    for (infoSourceIndex, errorInfoSource) in errorInfoSources.enumerated() {
+      let errorInfo = errorInfoSource[keyPath: errorInfoKeyPath]
+      for (key, value) in errorInfo._storage {
+        // 3. Collisions
+        // 3.1 If there is cross collision (the same key in info of several errors), then sourceSignature (e.g. error domain + code)
+        // is added to key. If several errors have the same signature, then source index from errorInfoSources is also added.
+        // In such a way all keys across errorInfoSources become unique.
+        // 3.2 If the same key is met several times inside an errorInfo, then collisionSource interpolation is added to a key.
+        // If there are equal collision sources for the same key (e.g. `.onSubscript`), a random suffix
+        // will be added (by _putResolvingWithRandomSuffix() func).
+        
+        var augmentedKey = _StatefulKey(key)
+        
+        let shouldAddKeyKind = false
+        if shouldAddKeyKind {
+          // add key tag according to options
+        }
+        
+        let hasCrossErrorsCollision = crossCollisionKeys.contains(key)
+        if hasCrossErrorsCollision {
+          let sourceSignature = _unchecked_infoSourceSignatureForCrossCollision(infoSourceIndex: infoSourceIndex,
+                                                                                allSourcesSignatures: allSourcesSignatures)
+          augmentedKey.append(sourceSignature)
+        }
+        
+        // TODO: In this kind of summary-merge it is reasonable to provide an option if nil values with different Optional.Wrapped
+        // types should be put to summary.
+        // TODO: add KeyKind.shortSign()
+        // TODO: collisionSource arg unused
+        
+        // will be multiple values for key later when MultivalueDict types used
+        // TODO: use a slice or view to prevent heap allocation
+        // TODO: after removingEqualValues there may be collisionSources, e.g. after  removingEqualValues there will be only
+        // 1 value with collisionSource. This collisionSource can be skipped as the value is unique and result dictionary
+        // will not contain approx. equal values. However, in general case this collisionSource should still be attached to the key
+        // for handling the fact that collision occured. The total elimination of collisionSource can be done by passing additional
+        // argument or option to this function
+  //      let processedValues = prepareValues(NonEmptyArray(value), removingEqualValues: omitEqualValues)
+        // TODO: processedValues contain all values for key which leads to incorrect ordering.
+        
+        if let collisionSource = value.collisionSource {
+          let collisionString = collisionSourceInterpolation(collisionSource)
+          augmentedKey.append(collisionString)
+        }
+        // value collisions within concrete error instance | crossCollisions
+        let adaptedValue = valueTransform(value.value)
+        putResolvingCollisions(key: augmentedKey.string, value: adaptedValue)
+      } // end `for (key, value)`
+    } // end `for (errorIndex, error)`
+    
+    return summary
   }
-  
-  // 1. Find collisions across errorInfo sources (typically it is errors)
-  let crossCollisionKeys = findCommonElements(across: errorInfoSources.map { $0[keyPath: errorInfoKeyPath].uniqueKeys })
-  
-  // 2. Iterate over key-value pairs in each errorInfo source.
-  lazy var allSourcesSignatures = errorInfoSources.map(errorSignatureBuilder)
-  for (infoSourceIndex, errorInfoSource) in errorInfoSources.enumerated() {
-    let errorInfo = errorInfoSource[keyPath: errorInfoKeyPath]
-    for (key, value) in errorInfo._storage {
-      let hasCrossErrorsCollision = crossCollisionKeys.contains(key)
-      
-      // 3. Collisions
-      // 3.1 If there is cross collision (the same key in info of several errors), then sourceSignature (e.g. error domain + code)
-      // is added to key. If several errors have the same signature, then source index from errorInfoSources is also added.
-      // In such a way all keys across errorInfoSources become unique.
-      // 3.2 If the same key is met several times inside an errorInfo, then collisionSource interpolation is added to a key.
-      // If there are equal collision sources for the same key (e.g. `.onSubscript`), a random suffix
-      // will be added (by _putResolvingWithRandomSuffix() func).
-      
-      var augmentedKey = _StatefulKey(key)
-      
-      let shouldAddKeyKind = false
-      if shouldAddKeyKind {
-        // add key tag according to options
-      }
-      
-      if hasCrossErrorsCollision {
-        let sourceSignature = _unchecked_infoSourceSignatureForCrossCollision(infoSourceIndex: infoSourceIndex,
-                                                                              allSourcesSignatures: allSourcesSignatures)
-        augmentedKey.append(sourceSignature)
-      }
-      
-      // TODO: In this kind of summary-merge it is reasonable to provide an option if nil values with different Optional.Wrapped
-      // types should be put to summary.
-      // TODO: add KeyKind.shortSign()
-      // TODO: collisionSource arg unused
-      
-      // will be multiple values for key later when MultivalueDict types used
-      // TODO: use a slice or view to prevent heap allocation
-      // TODO: after removingEqualValues there may be collisionSources, e.g. after  removingEqualValues there will be only
-      // 1 value with collisionSource. This collisionSource can be skipped as the value is unique and result dictionary
-      // will not contain approx. equal values. However, in general case this collisionSource should still be attached to the key
-      // for handling the fact that collision occured. The total elimination of collisionSource can be done by passing additional
-      // argument or option to this function
-//      let processedValues = prepareValues(NonEmptyArray(value), removingEqualValues: omitEqualValues)
-      // TODO: processedValues contain all values for key which leads to incorrect ordering.
-      
-      if let collisionSource = value.collisionSource {
-        let collisionString = collisionSourceInterpolation(collisionSource)
-        augmentedKey.append(collisionString)
-      }
-      // value collisions within concrete error instance | crossCollisions
-      let adaptedValue = valueTransformer(value.value)
-      putResolvingCollisions(key: augmentedKey.string, value: adaptedValue)
-    } // end `for (key, value)`
-  } // end `for (errorIndex, error)`
-  
-  return summary
 }
+
+
 
 fileprivate struct _StatefulKey {
   private(set) var string: String
