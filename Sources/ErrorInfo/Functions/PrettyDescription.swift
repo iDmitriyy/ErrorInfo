3e543c41798600cb598c4e267d5734f5620e9423
Dmitriy Ignatyev <21144498+iDmitriyy@users.noreply.github.com>
2025-12-11 21:54:54 +0300

    remove Foundation from prettyDescriptionOfOptional imp.

--- Sources/ErrorInfo/Functions/PrettyDescription.swift
+++ Sources/ErrorInfo/Functions/PrettyDescription.swift
@@ -5,23 +5,25 @@
 //  Created by Dmitriy Ignatyev on 14.12.2024.
 //
 
-private import Foundation
-
-// TODO: - remove foundation
-
 // MARK: - Pretty Description for Any & Optional<T>
 
-/// Cleans up the string representation of any value (including deeply nested optionals) by removing redundant "Optional(...)" wrappers.
+/// Returns a string representation of any value removing redundant `Optional( )` wrappers.
+///
+/// It removes all occurrences of `Optional(...)` from the string representation, leaving only the unwrapped value.
+/// If the value is `nil`, it returns `"nil"`.
+/// It works with any values, including deeply nested optionals.
 ///
 /// # Example:
 /// ```swift
 /// prettyDescriptionOfOptional(any: Optional(Optional(42)))        // "42"
 /// prettyDescriptionOfOptional(any: Optional<Optional<Int>>.none)  // "nil"
 /// prettyDescriptionOfOptional(any: "Hello")                       // "Hello"
+/// prettyDescriptionOfOptional(any: Optional("World"))             // "World"
 /// ```
-/// - Parameter any: A value of type `Any`, which may or may not be an optional.
-/// - Returns: A String representing the unwrapped value or "nil"
-public func prettyDescriptionOfOptional<T>(any: T) -> String {
+///
+/// - Parameter any: A value of any type, which may or may not be an optional.
+/// - Returns: A string representation of the unwrapped value, or `"nil"` if the value is `nil`.
+public func prettyDescriptionOfOptional(any: some Any) -> String {
   if let value = _specialize(any, for: String.self) { return value }
   if let value = _specialize(any, for: Int.self) { return String(value) }
   if let value = _specialize(any, for: UInt.self) { return String(value) }
@@ -42,19 +44,29 @@ public func prettyDescriptionOfOptional<T>(any: T) -> String {
   return isModified ? String(intermediate) : description
 } // inlining has no effect on perfomance
 
-/// Cleans up the string representation of any value (including deeply nested optionals) by removing redundant "Optional(...)" wrappers.
+/// Returns a string representation of an optional value removing redundant `Optional( )` wrappers.
+///
+/// It removes all occurrences of `Optional(...)` from the string representation, leaving only the unwrapped value.
+/// If the value is `nil`, it returns `"nil"`.
+/// It works with any values, including deeply nested optionals.
 ///
 /// # Example:
 /// ```swift
-/// prettyDescriptionOfOptional(any: Optional(Optional(42)))        // "42"
-/// prettyDescriptionOfOptional(any: Optional<Optional<Int>>.none)  // "nil"
-/// prettyDescriptionOfOptional(any: "Hello")                       // "Hello"
+/// prettyDescriptionOfOptional(any: Optional(42))         // "42"
+/// prettyDescriptionOfOptional(any: nil as Optional<Int>) // "nil"
+/// prettyDescriptionOfOptional(any: Optional("Hello"))    // "Hello"
 /// ```
-/// - Parameter any: A value of type `T?`, which may or may not be an optional.
-/// - Returns: A String representing the unwrapped value or "nil"
+///
+/// - Parameter any: An optional value of any type, which may or may not be `nil` or wrapped in multiple `Optional` layers.
+/// - Returns: A string representation of the unwrapped value, or `"nil"` if the value is `nil`.
 public func prettyDescriptionOfOptional<T>(any: T?) -> String {
   switch any {
   case .some(let value):
+    /*
+     if let value = _specialize(any, for: String.self) { return value } – _specialize has no effect here.
+     However, there is significant perfomance gain when using _specialize
+     inside any T overload `prettyDescriptionOfOptional()`
+     */
     return prettyDescriptionOfOptional(any: value)
     
   case .none:
